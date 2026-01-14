09e76f8dd76299d4c6ca687cca4bd4874f9c5ddf
nimo455 <faizamaalim456@gmail.com>
2026-01-14 11:43:33 +0300

    done

---
+++ index.js
@@ -0,0 +1,39 @@
+function string() {
+    return string.toUpperCase();
+  }
+
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate(string) {
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!";
+  } else if (string === string.toUpperCase()) {
+    return "YES INDEED!";
+  } else if (string === "Let's have dinner together!") {
+    return "I would love to!";
+  }
+}
+
+if (typeof module !== 'undefined') {
+  module.exports = {
+    shout,
+    whisper,
+    logShout,
+    logWhisper,
+    sayHiToHeadphonedRoommate
+  };
+}
